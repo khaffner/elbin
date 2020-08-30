6c7650ceb7f0c5823babba8c9e560de9cffd849f
Kevin Haffner <khaffner@gmail.com>
2020-08-30 23:55:49 +0200

    ...

--- LogSenseHat.py
+++ LogSenseHat.py
@@ -19,7 +19,7 @@ while(True):
     # https://projects-static.raspberrypi.org/projects/generic-theory-pitch-roll-yaw/1da6c9e518533fe8c1f70d7445fd6880d7dac12a/en/images/orientation.png
 
     x = {
-        "timestamp": datetime.datetime.now().isoformat()
+        "timestamp": datetime.datetime.now().isoformat(),
         "compass": compass,
         "pitch": pitch,
         "roll": roll,
