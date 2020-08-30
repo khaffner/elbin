ff65bc3a4ce4d1f3c6094c7218529d07f294fb6e
Kevin Haffner <khaffner@gmail.com>
2020-08-30 23:32:45 +0200

    new script

--- LogSenseHat.py
+++ LogSenseHat.py
@@ -2,6 +2,20 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-temp = sense.get_temperature_from_humidity()
-print("Temperature: %s C" % temp)
-sense.show_message("Hello World!")
+while(True):
+    # Enable only compass
+    sense.set_imu_config(True,False,False)
+    compass = sense.get_compass() # float
+
+    # Enable only gyro
+    sense.set_imu_config(False,True,False)
+    gyro = sense.get_gyroscope()
+    # pitch, roll, yaw. 
+    # https://projects-static.raspberrypi.org/projects/generic-theory-pitch-roll-yaw/1da6c9e518533fe8c1f70d7445fd6880d7dac12a/en/images/orientation.png
+
+    print("-----------------------------")
+    print(compass)
+    print("-----------------------------")
+    print(gyro)
+
+    time.Sleep(60)
