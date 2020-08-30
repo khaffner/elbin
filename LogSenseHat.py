d915f6e17d8628b67bcf6f8cf95b470fac89a1c8
Kevin Haffner <khaffner@gmail.com>
2020-08-30 23:43:51 +0200

    extract values

--- LogSenseHat.py
+++ LogSenseHat.py
@@ -11,12 +11,16 @@ while(True):
     # Enable only gyro
     sense.set_imu_config(False,True,False)
     gyro = sense.get_gyroscope()
-    # pitch, roll, yaw. 
+    pitch = "{pitch}".format(**gyro)
+    roll = "{roll}".format(**gyro)
+    yaw = "{yaw}".format(**gyro)
     # https://projects-static.raspberrypi.org/projects/generic-theory-pitch-roll-yaw/1da6c9e518533fe8c1f70d7445fd6880d7dac12a/en/images/orientation.png
 
     print("-----------------------------")
     print(compass)
     print("-----------------------------")
-    print(gyro)
+    print(pitch)
+    print(roll)
+    print(yaw)
 
     time.sleep(60)
