0bc4893cb16e74f1d8282df9706017f1276e2e17
Kevin Haffner <khaffner@gmail.com>
2020-09-03 22:55:01 +0200

    must be string value

--- LogSenseHat.py
+++ LogSenseHat.py
@@ -4,10 +4,11 @@ import time
 sense = SenseHat()
 
 while(True):
+    orientation = sense.get_orientation()
     # https://projects-static.raspberrypi.org/projects/generic-theory-pitch-roll-yaw/1da6c9e518533fe8c1f70d7445fd6880d7dac12a/en/images/orientation.png
 
     outF = open("/logs/sensehat.log", "a")
-    outF.write(sense.get_orientation())
+    outF.write(orientation)
     outF.write("\n")
     outF.close()
 
