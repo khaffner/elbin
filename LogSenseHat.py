8ecb9519d8df36cee8e3eae26ca86b4185516fa7
Kevin Haffner <khaffner@gmail.com>
2020-09-03 23:17:12 +0200

    use python just for sense hat

--- LogSenseHat.py
+++ LogSenseHat.py
@@ -1,20 +1,9 @@
 from sense_hat import SenseHat
-import time
 import json
 
 sense = SenseHat()
 
-while(True):
-    orientation = sense.get_orientation()
-    # https://projects-static.raspberrypi.org/projects/generic-theory-pitch-roll-yaw/1da6c9e518533fe8c1f70d7445fd6880d7dac12a/en/images/orientation.png
-
-    orientationString = json.dumps(orientation)
-
-    print(orientationString)
-
-    outF = open("/logs/sensehat.log", "a")
-    outF.write(orientationString)
-    outF.write("\n")
-    outF.close()
-
-    time.sleep(1)
+# https://projects-static.raspberrypi.org/projects/generic-theory-pitch-roll-yaw/1da6c9e518533fe8c1f70d7445fd6880d7dac12a/en/images/orientation.png
+orientation = sense.get_orientation()
+orientationString = json.dumps(orientation)
+print(orientationString)
