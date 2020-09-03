092044bafa6dca84654b5441bc46819c6a2f5594
Kevin Haffner <khaffner@gmail.com>
2020-09-03 23:03:13 +0200

    as string

--- LogSenseHat.py
+++ LogSenseHat.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat
 import time
+import json
 
 sense = SenseHat()
 
@@ -7,8 +8,10 @@ while(True):
     orientation = sense.get_orientation()
     # https://projects-static.raspberrypi.org/projects/generic-theory-pitch-roll-yaw/1da6c9e518533fe8c1f70d7445fd6880d7dac12a/en/images/orientation.png
 
+    orientationString = json.dumps(orientation)
+
     outF = open("/logs/sensehat.log", "a")
-    outF.write(orientation)
+    outF.write(orientationString)
     outF.write("\n")
     outF.close()
 
