fb9ddd403a1bc96c2aacd671ebe4756953ba538c
Kevin Haffner <khaffner@gmail.com>
2020-09-06 19:47:50 +0200

    more practical values

--- LogOrientation.ps1
+++ LogOrientation.ps1
@@ -1,8 +1,10 @@
 class Orientation {
     [datetime]$timestamp
-    [float]$pitch
-    [float]$yaw
-    [float]$roll
+    [int]$pitch
+    [int]$yaw
+    [int]$roll
+    [int]$rollFriendly
+    [string]$rollDirection = "Centered"
 }
 
 while($true) {
@@ -13,7 +15,17 @@ while($true) {
     $obj.pitch = $src.pitch
     $obj.yaw = $src.yaw #Compass
     $obj.roll = $src.roll
+    $obj.rollFriendly = $src.roll
+
+    if ($obj.rollFriendly -lt 180) {
+        $obj.rollDirection = "Right"
+    }
+    
+    if($obj.rollFriendly -gt 180) {
+        $obj.rollDirection = "Left"
+        $obj.rollFriendly = 360 - $obj.roll #Flipped
+    }
 
     Write-Log -SensorName "orientation" -Text ($obj | ConvertTo-Json -Compress)
-    Start-Sleep -Seconds 10
+    Start-Sleep -Seconds 1
 }
