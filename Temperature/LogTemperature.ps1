cd8f543566fc9ba7df275a67bf4a035588c25113
Kevin Haffner <khaffner@gmail.com>
2020-09-07 23:10:55 +0200

    specify water temperature

--- Temperature/LogTemperature.ps1
+++ Temperature/LogTemperature.ps1
@@ -1,6 +1,6 @@
 class Temperature {
     [datetime]$timestamp
-    [string]$temp
+    [string]$water
 }
 digitemp_DS9097 -i -s /dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0 -c /etc/digitemp.conf
 while($true) {
@@ -8,7 +8,7 @@ while($true) {
 
     $obj = New-Object Temperature
     $obj.timestamp = Get-Date -Format "o" # ISO 8601
-    $obj.temp = $raw.Split(": ")[-2].Replace(" F","")
+    $obj.water = $raw.Split(": ")[-2].Replace(" F","")
 
     Write-Log -SensorName "temperature" -Text ($obj | ConvertTo-Json -Compress)
     Start-Sleep -Seconds 59
