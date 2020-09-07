af9d73478b8b53c396d16e32e20dd90f3a4d9676
Kevin Haffner <khaffner@gmail.com>
2020-09-07 23:57:27 +0200

    make 360 0

--- Orientation/LogOrientation.ps1
+++ Orientation/LogOrientation.ps1
@@ -11,13 +11,17 @@ class Orientation {
 
 while($true) {
     $src = python /app/Orientation.py | ConvertFrom-Json
-
+    
     $obj = New-Object Orientation
     $obj.timestamp = Get-Date -Format "o" # ISO 8601
     $obj.yaw = $src.yaw #Compass
+
     $obj.pitch = $src.pitch
+    if($obj.pitch -eq 360) {$obj.pitch = 0}
     $obj.pitchFriendly = $obj.pitch
+    
     $obj.roll = $src.roll
+    if($obj.roll -eq 360) {$obj.roll = 0}
     $obj.rollFriendly = $obj.roll
 
     # Friendlier values for roll (sideways)
